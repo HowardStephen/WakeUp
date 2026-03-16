1b090f1dde5716947b2c65a5af41b49b4746d8db
Howard <HowardHenryStephen@gmail.com>
2026-03-16 11:25:13 +0800

    Initial Commit

---
+++ WakeUp Watch App/WakeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpApp.swift
+//  WakeUp Watch App
+//
+//  Created by Henry Stephen on 2026/3/16.
+//
+
+import SwiftUI
+
+@main
+struct WakeUp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
